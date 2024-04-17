936aace7aaa30bc61d0e009fb68a33c3cb69bb25
Konstantin Savusia <savconstantine@gmail.com>
2024-04-18 05:53:45 +0600

    add typescript dependecies

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,17 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  env: {
-    node: true,
-  },
+  root: true,
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "@vue/eslint-config-prettier",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier/skip-formatting",
   ],
   parserOptions: {
     ecmaVersion: "latest",
   },
   rules: {
-    // override/add rules settings here, such as:
-    // 'vue/no-unused-vars': 'error'
+    "vue/multi-word-component-names": 0,
   },
 };
